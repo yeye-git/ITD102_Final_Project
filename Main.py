28f1a2994905ecfe2f297a5fa4000ccade8200cf
yeye-git <59961856+yeye-git@users.noreply.github.com>
2020-01-19 18:26:39 +1000

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,21 @@
+import os
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+
+print("taking")
+camera.start_preview()
+sleep(5)
+camera.capture("input.jpg")
+camera.stop_preview()
+
+print("recog")
+
+# get c# result
+stream = os.popen("dotnet run input.jpg")
+output = stream.read()
+if(output =="1\n"):
+    os.system("python move.py")
+else:
+    print("not!")
